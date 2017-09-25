3c37b2c0ca64cacb6034ae811fcb8f7f5f84ea05
JCROGAR <jorgegr7774@gmail.com>
2017-09-24 23:33:01 -0500

    commit 23:32

--- app/src/main/java/wcm/jcrogar/fca/control/login.java
+++ app/src/main/java/wcm/jcrogar/fca/control/login.java
@@ -1,13 +1,67 @@
 package wcm.jcrogar.fca.control;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
+
+
+    private EditText UserEdit;
+    private EditText PassEdit;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        UserEdit = (EditText) findViewById(R.id.LoginUser);
+        PassEdit    = (EditText) findViewById(R.id.LoginPass);
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Toast.makeText(login.this, "El usuario inicio sesion", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(login.this, "El usuario salio de la sesion", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        };
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+       firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(authStateListener != null)
+            firebaseAuth.removeAuthStateListener(authStateListener);
+
+
+    }
+
+    public void loginOn(View view) {
+        Intent intentlogin = new Intent(this, access.class);
+        startActivity(intentlogin);
+        Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show();
     }
 }
